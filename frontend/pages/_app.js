e04cf019810b284ee472cdaf9f23c7cf208f7289
AHILL-0121 <your-email@example.com>
2026-03-02 16:16:22 +0530

    fix: gradient text invisible in Firefox + load Inter 900 weight

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../public/fonts.css';
 import '../public/glassmorphism.css';
 import 'leaflet/dist/leaflet.css';
 
